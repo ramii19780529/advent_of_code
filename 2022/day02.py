a830cdd99d378ea8c83d0dcc6ebc60883094f6ba
Robert A Moreno II <ramii19780529@gmail.com>
2022-12-02 01:57:15 -0500

    Changed the method used to parse the input.

--- 2022/day02.py
+++ 2022/day02.py
@@ -9,27 +9,6 @@ rps_shapes = {
     "Y": "Paper",
     "Z": "Scissors",
 }
-
-
-def score1(rps):
-    p1, p2 = rps.split()
-    if rps_shapes[p2] == "Rock":
-        _score = 1
-        if rps_shapes[p1] == "Scissors":
-            _score += 6
-    if rps_shapes[p2] == "Paper":
-        _score = 2
-        if rps_shapes[p1] == "Rock":
-            _score += 6
-    if rps_shapes[p2] == "Scissors":
-        _score = 3
-        if rps_shapes[p1] == "Paper":
-            _score += 6
-    if rps_shapes[p1] == rps_shapes[p2]:
-        _score += 3
-    return _score
-
-
 rps_response = {
     "A": ["C", "A", "B"],
     "B": ["A", "B", "C"],
@@ -39,14 +18,33 @@ rps_response = {
 rps_resp_idx = {"X": 0, "Y": 1, "Z": 2}
 
 
+def score1(rps):
+    if rps_shapes[rps[1]] == "Rock":
+        _score = 1
+        if rps_shapes[rps[0]] == "Scissors":
+            _score += 6
+    if rps_shapes[rps[1]] == "Paper":
+        _score = 2
+        if rps_shapes[rps[0]] == "Rock":
+            _score += 6
+    if rps_shapes[rps[1]] == "Scissors":
+        _score = 3
+        if rps_shapes[rps[0]] == "Paper":
+            _score += 6
+    if rps_shapes[rps[0]] == rps_shapes[rps[1]]:
+        _score += 3
+    return _score
+
+
 def score2(rps):
-    p1, p2 = rps.split()
-    p2 = rps_response[p1][rps_resp_idx[p2]]
-    return score1(p1 + " " + p2)
+    rps[1] = rps_response[rps[0]][rps_resp_idx[rps[1]]]
+    return score1(rps)
 
 
 # Parse the puzzle input file.
 with open("advent_of_code\\2022\\day02.txt") as puzzle_input:
-    encrypted_strategy_guide = puzzle_input.read().splitlines()
+    encrypted_strategy_guide = [
+        [shape[0], shape[2]] for shape in puzzle_input.read().splitlines()
+    ]
     print(sum(map(score1, encrypted_strategy_guide)))
     print(sum(map(score2, encrypted_strategy_guide)))
