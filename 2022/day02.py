17590aca9b722b5e86cd97a547891f43eb422d86
Robert A Moreno II <ramii19780529@gmail.com>
2022-12-02 10:58:24 -0500

    Changed code to solve using maths instead.

--- 2022/day02.py
+++ 2022/day02.py
@@ -1,51 +1,8 @@
 # Day 2: Rock Paper Scissors
 # https://adventofcode.com/2022/day/2
 
-rps_shapes = {
-    "A": "Rock",
-    "B": "Paper",
-    "C": "Scissors",
-    "X": "Rock",
-    "Y": "Paper",
-    "Z": "Scissors",
-}
-
-rps_response = {
-    "A": ["C", "A", "B"],
-    "B": ["A", "B", "C"],
-    "C": ["B", "C", "A"],
-}
-
-rps_resp_idx = {"X": 0, "Y": 1, "Z": 2}
-
-
-def score1(rps):
-    if rps_shapes[rps[1]] == "Rock":
-        _score = 1
-        if rps_shapes[rps[0]] == "Scissors":
-            _score += 6
-    if rps_shapes[rps[1]] == "Paper":
-        _score = 2
-        if rps_shapes[rps[0]] == "Rock":
-            _score += 6
-    if rps_shapes[rps[1]] == "Scissors":
-        _score = 3
-        if rps_shapes[rps[0]] == "Paper":
-            _score += 6
-    if rps_shapes[rps[0]] == rps_shapes[rps[1]]:
-        _score += 3
-    return _score
-
-
-def score2(rps):
-    rps[1] = rps_response[rps[0]][rps_resp_idx[rps[1]]]
-    return score1(rps)
-
-
 # Parse the puzzle input file.
 with open("advent_of_code\\2022\\day02.txt") as puzzle_input:
-    encrypted_strategy_guide = [
-        [shape[0], shape[2]] for shape in puzzle_input.read().splitlines()
-    ]
-    print(sum(map(score1, encrypted_strategy_guide)))
-    print(sum(map(score2, encrypted_strategy_guide)))
+    esg = [[ord(r[0]) - 64, ord(r[2]) - 87] for r in puzzle_input.readlines()]
+    print(sum((p2 + 1 - p1) % 3 * 3 + p2 for p1, p2 in esg))
+    print(sum((p2 + p1) % 3 + 1 + (p2 - 1) * 3 for p1, p2 in esg))
