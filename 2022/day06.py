a0591162f7d18decf2419e1ca1474f4de31ea233
Robert A Moreno II <ramii19780529@gmail.com>
2022-12-06 07:34:21 -0500

    Completed day 6.

---
+++ 2022/day06.py
@@ -0,0 +1,17 @@
+# Day 6: Tuning Trouble
+# https://adventofcode.com/2022/day/6
+
+
+def find_start(datastream, start, length):
+    for i in range(start, len(datastream) - length):
+        if len(set(datastream[i : i + length])) == length:
+            print(i + length)
+            break
+    return i + length
+
+
+# Parse the puzzle input file.
+with open("advent_of_code\\2022\\day06.txt") as puzzle_input:
+    datastream = puzzle_input.read()
+    part1 = find_start(datastream, 0, 4)
+    part2 = find_start(datastream, part1, 14)
