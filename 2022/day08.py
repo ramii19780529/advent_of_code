159a497e51e111d2e15b01352aa0103fd359bdcb
Robert A Moreno II <ramii19780529@gmail.com>
2022-12-09 23:43:23 -0500

    Simplified IF statements.

--- 2022/day08.py
+++ 2022/day08.py
@@ -8,16 +8,16 @@ with open("advent_of_code\\2022\\day08.txt") as puzzle_input:
     treemap = [[*map(int, line)] for line in puzzle_input.read().splitlines()]
     visible = 0
     scenic = 0
-    for y, _ in enumerate(treemap):
-        for x, height in enumerate(treemap[y]):
+    for y, row in enumerate(treemap):
+        for x, height in enumerate(row):
             # Part 1
             if y == 0 or y == len(treemap) - 1:
                 visible += 1
-            elif x == 0 or x == len(treemap[y]) - 1:
+            elif x == 0 or x == len(row) - 1:
                 visible += 1
-            elif max(treemap[y][:x]) < height:
+            elif max(row[:x]) < height:
                 visible += 1
-            elif max(treemap[y][x + 1 :]) < height:
+            elif max(row[x + 1 :]) < height:
                 visible += 1
             elif max(tree[x] for tree in treemap[:y]) < height:
                 visible += 1
@@ -25,24 +25,22 @@ with open("advent_of_code\\2022\\day08.txt") as puzzle_input:
                 visible += 1
             # Part 2
             views = [0, 0, 0, 0]
-            for i in range(x + 1, len(treemap[y])):
+            for i in row[x + 1 :]:
                 views[0] += 1
-                if treemap[y][i] >= height:
+                if i >= height:
                     break
-            for i in range(x - 1, -1, -1):
-                if i >= 0:
-                    views[1] += 1
-                    if treemap[y][i] >= height:
-                        break
-            for i in range(y + 1, len(treemap)):
+            for i in row[x - 1 :: -1]:
+                views[1] += 1
+                if i >= height:
+                    break
+            for i in [row[x] for row in treemap[y + 1 :]]:
                 views[2] += 1
-                if treemap[i][x] >= height:
+                if i >= height:
                     break
-            for i in range(y - 1, -1, -1):
-                if i >= 0:
-                    views[3] += 1
-                    if treemap[i][x] >= height:
-                        break
+            for i in [row[x] for row in treemap[y - 1 :: -1]]:
+                views[3] += 1
+                if i >= height:
+                    break
             if prod(views) > scenic:
                 scenic = prod(views)
 
