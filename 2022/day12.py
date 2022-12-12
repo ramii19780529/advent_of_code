716059179f5bec757b5dfa3048889c3cbd8bd9f9
Robert A Moreno II <ramii19780529@gmail.com>
2022-12-12 14:22:49 -0500

    Reduced memory usage and simplified IF statements.

--- 2022/day12.py
+++ 2022/day12.py
@@ -2,69 +2,52 @@
 # https://adventofcode.com/2022/day/12
 
 from heapq import heappop, heappush
-import copy
 
 
-def diff(a, b, part):
-    # Get the difference between two numbers.
-    if part == 1:
-        return a - b
-    if part == 2:
-        return b - a
-
-
-def shortest_path(grid, part):
+def shortest_path(grid, start, ends, direction):
     # This function returns the shortest path in
     # the grid using a Dijkstra-like algorithm.
-
-    # Set the start and destination positions.
-    # Part 1, S is the start and E is the destination.
-    # Part 2, E is the start and any position that contains an "a" is the destination.
     next_node = []
-    destination = set()
     visited = set()
-    for y, line in enumerate(grid):
-        for x, height in enumerate(line):
-            if height == "S":
-                grid[y][x] = "a"
-                if part == 1:
-                    heappush(next_node, (0, (x, y)))
-                if part == 2:
-                    destination.add((x, y))
-            if height == "E":
-                grid[y][x] = "z"
-                if part == 1:
-                    destination.add((x, y))
-                if part == 2:
-                    heappush(next_node, (0, (x, y)))
-            if part == 2 and height == "a":
-                destination.add((x, y))
-
-    # Loop until we hit a destination.
+    heappush(next_node, (0, start))
+    # Loop until we hit an end.
     while True:
         steps, (x, y) = heappop(next_node)
         if (x, y) in visited:
             continue
-        if (x, y) in destination:
+        if (x, y) in ends:
             return steps
         visited.add((x, y))
         height = ord(grid[y][x])
-        if x > 0 and diff(ord(grid[y][x - 1]), height, part) <= 1:
-            heappush(next_node, (steps + 1, (x - 1, y)))
-        if x + 1 < len(grid[y]) and diff(ord(grid[y][x + 1]), height, part) <= 1:
-            heappush(next_node, (steps + 1, (x + 1, y)))
-        if y > 0 and diff(ord(grid[y - 1][x]), height, part) <= 1:
-            heappush(next_node, (steps + 1, (x, y - 1)))
-        if y + 1 < len(grid) and diff(ord(grid[y + 1][x]), height, part) <= 1:
-            heappush(next_node, (steps + 1, (x, y + 1)))
+        for nx, ny in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
+            if 0 <= ny < len(grid):
+                if 0 <= nx < len(grid[ny]):
+                    if (ord(grid[ny][nx]) - height) * direction <= 1:
+                        heappush(next_node, (steps + 1, (nx, ny)))
 
 
 # Parse the puzzle input file.
 with open("advent_of_code\\2022\\day12.txt") as puzzle_input:
     height_map = [list(row) for row in puzzle_input.read().splitlines()]
+    # Set the start and end positions.
+    # Part 1, S is the start and E is the end.
+    # Part 2, E is the start and any position that contains an "a" is the end.
+    starts = [None, None]
+    ends = [set(), set()]
+    for y, line in enumerate(height_map):
+        for x, height in enumerate(line):
+            if height == "S":
+                height_map[y][x] = "a"
+                starts[0] = (x, y)
+            if height == "E":
+                height_map[y][x] = "z"
+                starts[1] = (x, y)
+                ends[0].add((x, y))
+            if height_map[y][x] == "a":
+                ends[1].add((x, y))
 
 # Part one.
-print(shortest_path(copy.deepcopy(height_map), 1))
+print(shortest_path(height_map, starts[0], ends[0], 1))
 
 # Part two.
-print(shortest_path(copy.deepcopy(height_map), 2))
+print(shortest_path(height_map, starts[1], ends[1], -1))
