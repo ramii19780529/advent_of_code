c004baba8cbf547fc5b77c9b40625c5fbed2f6f7
Robert A Moreno II <ramii19780529@gmail.com>
2022-12-29 22:21:17 -0500

    Updated paths for txt files.

---
+++ 2015/day07.py
@@ -0,0 +1,6 @@
+# Day 7: Some Assembly Required
+# https://adventofcode.com/2015/day/7
+
+# Parse the puzzle input file.
+with open("advent_of_code\\2015\\day06.txt") as puzzle_input:
+    instructions = [puzzle_input.read().splitlines()]
